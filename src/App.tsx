e184782fa0753db065aadacf2ecce30b820b2e2f
Xipu Li <lixipu98@gmail.com>
2022-07-27 17:37:20 -0700

    Add and integrate Edge SDK

--- src/App.tsx
+++ src/App.tsx
@@ -1,115 +1,47 @@
+import { EdgeAccount, EdgeContext, MakeEdgeContext } from 'edge-core-js'
+import { LoginScreen, LoginUiProvider } from 'edge-login-ui-rn'
+import * as React from 'react'
+import { Alert, Text } from 'react-native'
+
+import MainUI from './MainUI'
+
 /**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * Generated with the TypeScript template
- * https://github.com/react-native-community/react-native-template-typescript
- *
- * @format
+ * Makes the Edge context and Login UI modal provider
+ * available to the rest of the app.
  */
-
-import React from 'react';
-import {
-  SafeAreaView,
-  ScrollView,
-  StatusBar,
-  StyleSheet,
-  Text,
-  useColorScheme,
-  View,
-} from 'react-native';
-
-import {
-  Colors,
-  DebugInstructions,
-  Header,
-  LearnMoreLinks,
-  ReloadInstructions,
-} from 'react-native/Libraries/NewAppScreen';
-
-const Section: React.FC<{
-  title: string;
-}> = ({children, title}) => {
-  const isDarkMode = useColorScheme() === 'dark';
-  return (
-    <View style={styles.sectionContainer}>
-      <Text
-        style={[
-          styles.sectionTitle,
-          {
-            color: isDarkMode ? Colors.white : Colors.black,
-          },
-        ]}>
-        {title}
-      </Text>
-      <Text
-        style={[
-          styles.sectionDescription,
-          {
-            color: isDarkMode ? Colors.light : Colors.dark,
-          },
-        ]}>
-        {children}
-      </Text>
-    </View>
-  );
-};
-
 const App = () => {
-  const isDarkMode = useColorScheme() === 'dark';
-
-  const backgroundStyle = {
-    backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
-  };
-
+  // Stores the Edge context:
+  const [edgeContext, setEdgeContext] = React.useState<EdgeContext>()
+  const [edgeAccount, setEdgeAccount] = React.useState<EdgeAccount>()
+  const showError = (error: unknown) =>
+    Alert.alert('Error', String(error), [{ text: 'OK' }])
+  const handleLogout = () => {
+    edgeAccount?.logout()
+    setEdgeAccount(undefined)
+  }
   return (
-    <SafeAreaView style={backgroundStyle}>
-      <StatusBar barStyle={isDarkMode ? 'light-content' : 'dark-content'} />
-      <ScrollView
-        contentInsetAdjustmentBehavior="automatic"
-        style={backgroundStyle}>
-        <Header />
-        <View
-          style={{
-            backgroundColor: isDarkMode ? Colors.black : Colors.white,
-          }}>
-          <Section title="Step One">
-            Edit <Text style={styles.highlight}>App.tsx</Text> to change this
-            screen and then come back to see your edits.
-          </Section>
-          <Section title="See Your Changes">
-            <ReloadInstructions />
-          </Section>
-          <Section title="Debug">
-            <DebugInstructions />
-          </Section>
-          <Section title="Learn More">
-            Read the docs to discover what to do next:
-          </Section>
-          <LearnMoreLinks />
-        </View>
-      </ScrollView>
-    </SafeAreaView>
-  );
-};
+    <LoginUiProvider>
+      <MakeEdgeContext
+        // Get this from our support team:
+        apiKey="0b5776a91bf409ac10a3fe5f3944bf50417209a0"
+        appId="com.mydomain.myapp"
+        // Called when the core is done loading:
+        onLoad={setEdgeContext}
+        onError={showError}
+      />
+      {edgeContext == null ? (
+        <Text>Loading...</Text>
+      ) : edgeAccount == null ? (
+        <LoginScreen
+          accountOptions={{}}
+          context={edgeContext}
+          onLogin={setEdgeAccount}
+        />
+      ) : (
+        <MainUI edgeAccount={edgeAccount} onLogout={handleLogout} />
+      )}
+    </LoginUiProvider>
+  )
+}
 
-const styles = StyleSheet.create({
-  sectionContainer: {
-    marginTop: 32,
-    paddingHorizontal: 24,
-  },
-  sectionTitle: {
-    fontSize: 24,
-    fontWeight: '600',
-  },
-  sectionDescription: {
-    marginTop: 8,
-    fontSize: 18,
-    fontWeight: '400',
-  },
-  highlight: {
-    fontWeight: '700',
-  },
-});
-
-export default App;
+export default App
